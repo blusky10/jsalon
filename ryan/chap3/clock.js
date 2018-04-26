4f5b5064170ad9ec04eae115a1b26101d4498633
Ryan Woo <hyunuk.woo@samsung.com>
2018-04-26 18:24:11 +0900

    clock hands-on

---
+++ ryan/chap3/clock.js
@@ -0,0 +1,63 @@
+const oneSecond = () => 1000
+const getCurrentTime = () => new Date()
+const clear = () => console.clear()
+const log = message => console.log(message)
+
+const abstractClockTime = date => 
+({
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds()
+})
+
+const civilianHours = clockTime =>
+({
+    ...clockTime,
+    hours: (clockTime.hours > 12) ? clockTime.hours - 12 : clockTime.hours
+})
+
+const appenAMPM = clockTime => 
+({
+    ...clockTime,
+    ampm: (clockTime.hours >= 12) ? "PM" : "AM"
+})
+
+const display = target => time => target(time)
+
+const formatClock = format => time =>
+format.replace("hh", time.hours)
+.replace("mm", time.minutes)
+.replace("ss", time.seconds)
+.replace("tt", time.ampm)
+
+const prependZero = key => clockTime =>
+({
+    ...clockTime,
+    [key]: (clockTime[key]<10) ? "0" + clockTime[key] : clockTime[key]
+})
+
+const convertToCivilianTime = clockTime =>
+compose(appendAMPM,civilianHours)(clockTime)
+
+const doubleDigits = civilianTime =>
+compose(
+    prependZero("hours"),
+    prependZero("minutes"),
+    prependZero("seconds")
+)(civilianTime)
+
+const startTicking = () => 
+setInterval(
+    compose(
+        clear,
+        getCurrentTime,
+        abstractClockTime,
+        convertToCivilianTime,
+        doubleDigits,
+        formatClock("hh:mm:ss tt"),
+        display(log)
+    ),
+    oneSecond()
+)
+
+startTicking()
