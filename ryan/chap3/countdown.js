8c1358e24770215eb5d4da6ab9f9f2a122f904c4
Ryan Woo <hyunuk.woo@samsung.com>
2018-04-26 17:50:40 +0900

    countdown hands-on

--- ryan/chap3/countdown.js
+++ ryan/chap3/countdown.js
@@ -10,3 +10,27 @@ const countdown = (value, fn, delay =1000) => {
 }
 
 countdown(10, value => console.log(value))
+
+const dan = {
+    type: "person",
+    data: {
+        gender: "male",
+        info: {
+            id: 22,
+            fullname: {
+                first: "Dan",
+                last: "Deacon"
+            }
+        }
+    }
+}
+
+const deepPick = (fields, object={}) => {
+    const [first, ...remaining] = fields.split(".")
+    return (remaining.length) ?
+    deepPick(remaining.join("."), object[first]) :
+    object[first]
+}
+
+console.log(deepPick("type", dan))
+console.log(deepPick("data.info.fullname.first", dan))
