685d40887e18153d8ee75ecd4b13cfec5da2ece4
SeongYul, BAE <sybae0306@gmail.com>
2018-06-02 23:54:37 +0900

    State and Lifecycle 예제 적용

--- sybae/learning-react/src/App.js
+++ sybae/learning-react/src/App.js
@@ -3,19 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Learning React (sybae)</h1>
+                </header>
+                <p className="App-intro">
+                    <del>To get started, edit <code>src/App.js</code> and save to reload.</del>
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
